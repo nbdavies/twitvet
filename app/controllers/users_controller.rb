fa9ad7fa09b418899219134d000f84975c61d346
Caroline <kenworthyc@gmail.com>
2016-02-04 14:45:54 -0600

    Add font, fix call to errors in route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 #return a HTML form for creating new users
 get '/users/new/?' do
+  @user = User.new
   erb :'users/new'
 end
 
 #create new users
 post '/users/?' do
   @user = User.new(params[:user])
+  @errors = @user.errors.full_messages
   if @user.save
     sessions[:user_id] = @user.id
     redirect to :'/'
